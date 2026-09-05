2aa18bf3d627d898c4020701f11bb2ccff0194f9
PaPaYang <82997587+PaPaYang@users.noreply.github.com>
2026-09-05 21:21:04 +0900

    Update ThumbnailProvider.h

--- ThumbnailProvider.h
+++ ThumbnailProvider.h
@@ -1,48 +1,31 @@
 #pragma once
-#include <shobjidl.h>
+#include <windows.h>
 #include <thumbcache.h>
-#include <atlbase.h>
-#include <atlcom.h>
 #include <wrl/client.h>
-#include "ModernArchiveThumbnail_h.h" 
+#include <string>
+#include <vector>
 
-#define THUMBNAIL_VERSION L"1.1.2"
+using Microsoft::WRL::ComPtr;
 
-#ifdef _DEBUG
-#define LOG_DEBUG(msg) OutputDebugStringW(L"[Thumbnail] " msg L"\n")
-#else
-#define LOG_DEBUG(msg)
-#endif
-
-using namespace Microsoft::WRL;
-
-class ATL_NO_VTABLE CThumbnailProvider :
-    public ATL::CComObjectRootEx<ATL::CComSingleThreadModel>,
-    public ATL::CComCoClass<CThumbnailProvider, &CLSID_ThumbnailProvider>,
-    public IInitializeWithStream,
-    public IThumbnailProvider
-{
+class CThumbnailProvider : public IInitializeWithStream, public IThumbnailProvider {
 public:
-    CThumbnailProvider() {}
+    CThumbnailProvider();
+    ~CThumbnailProvider();
 
-    DECLARE_REGISTRY_RESOURCEID()
+    // IUnknown
+    IFACEMETHODIMP QueryInterface(REFIID riid, void** ppv);
+    IFACEMETHODIMP_(ULONG) AddRef();
+    IFACEMETHODIMP_(ULONG) Release();
 
-    BEGIN_COM_MAP(CThumbnailProvider)
-        COM_INTERFACE_ENTRY(IInitializeWithStream)
-        COM_INTERFACE_ENTRY(IThumbnailProvider)
-    END_COM_MAP()
+    // IInitializeWithStream
+    IFACEMETHODIMP Initialize(IStream* pStream, DWORD grfMode);
 
-    DECLARE_PROTECT_FINAL_CONSTRUCT()
-
-    HRESULT FinalConstruct() { return S_OK; }
-    void FinalRelease() {}
-
-    STDMETHODIMP Initialize(IStream* pStream, DWORD grfMode);
-    STDMETHODIMP GetThumbnail(UINT cx, HBITMAP* phbmp, WTS_ALPHATYPE* pdwAlpha);
+    // IThumbnailProvider
+    IFACEMETHODIMP GetThumbnail(UINT cx, HBITMAP* phbmp, WTS_ALPHATYPE* pdwAlpha);
 
 private:
+    long m_cRef;
     ComPtr<IStream> m_spStream;
+
     HRESULT GetThumbnailImpl(UINT cx, HBITMAP* phbmp, WTS_ALPHATYPE* pdwAlpha);
 };
-
-OBJECT_ENTRY_AUTO(__uuidof(ThumbnailProvider), CThumbnailProvider)
